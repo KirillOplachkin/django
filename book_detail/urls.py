c9cc6fb71cea41c5b1923a3b6e358d2c2e2e7f55
KirillOplachkin <oplachkin228@gmail.com>
2022-02-18 21:39:55 +0600

    4hw

--- book_detail/urls.py
+++ book_detail/urls.py
@@ -5,6 +5,8 @@ app_name = "books"
 urlpatterns = [
     path('book_d/', views.get_book_all, name='book_list'),
     path('book_d/<int:id>/', views.get_book_detail, name='book_detail'),
+    path('book_d/<int:id>/update/', views.put_books_update, name='book_update'),
     path('add-books/', views.add_book, name='add_book'),
+    path('book_d/<int:id>/delete/', views.books_delete, name='book_delete'),
 ]
 
--- book_detail/views.py
+++ book_detail/views.py
@@ -4,20 +4,25 @@ from django.shortcuts import reverse, redirect
 from django.http import Http404, HttpResponse
 from . import models, forms
 
+
 def get_book_all(request):
     books = models.Books.objects.all().order_by("id")
     return render(request, "books_list.html", {"books": books})
+
+
 def get_book_detail(request, id):
     global comment
     try:
         book = get_object_or_404(models.Books, id=id)
         try:
-            comment = models.BookComment.objects.filter(book_id=id).order_by("created_date")
+            comment = models.BookComment.objects.filter(books_id=book).order_by("created_date")
         except models.Books.DoesNotExist:
             print('No comments')
     except models.Books.DoesNotExist:
         raise Http404('Books does not exist, try another id')
-    return render(request, "books_detail.html", dict(book=book, books_comment=comment))
+    return render(request, "book_detail.html", {"book": book,
+                                                "books_comment": comment})
+
 
 def add_book(request):
     method = request.method
@@ -30,3 +35,28 @@ def add_book(request):
     else:
         form = forms.BookForm()
     return render(request, "add_books.html", {"form": form})
+
+
+def put_books_update(request, id):
+    book_id = get_object_or_404(models.Books, id=id)
+    if request.method == "POST":
+        form = forms.BookForm(instance=book_id,
+                              data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse("books:book_list"))
+    else:
+        form = forms.BookForm(instance=book_id)
+    return render(request, "books_update.html", {"form": form,
+                                                 "book": book_id})
+
+def books_delete(request, id):
+    book_id = get_object_or_404(models.Books, id=id)
+    book_id.delete()
+    # return HttpResponse("Book deleted")
+    return redirect(reverse("book:book_list"))
+
+
+
+
+
